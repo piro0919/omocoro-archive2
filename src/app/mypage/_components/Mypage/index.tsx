6a50047e31f166b6257aac2d96ae5cbcbff88bec
piro0919 <piro.haniwa@gmail.com>
2025-01-04 05:36:19 +0900

    feat: refactor Mypage component layout and styles for improved UI
    - Restructured the Mypage component to include new article sections with headers for better organization.
    - Updated styles to enhance the layout, including a grid display for the wrapper and new article styling.
    - Added a new description class for improved text styling.
    - Enhanced checkbox functionality for user preferences regarding content visibility.

--- src/app/mypage/_components/Mypage/index.tsx
+++ src/app/mypage/_components/Mypage/index.tsx
@@ -22,32 +22,40 @@ export default function Mypage({ isNotOnigiri }: MypageProps): JSX.Element {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.inner}>
-        <Checkbox
-          onChange={(e) => {
-            setCookie("is-not-onigiri", e.currentTarget.checked, {
-              maxAge: 365 * 24 * 60 * 60,
-              sameSite: "lax",
-              secure: true,
-            });
+      <article className={styles.article}>
+        <div className={styles.inner}>
+          <h2 className={styles.h2}>設定</h2>
+          <Checkbox
+            onChange={(e) => {
+              setCookie("is-not-onigiri", e.currentTarget.checked, {
+                maxAge: 365 * 24 * 60 * 60,
+                sameSite: "lax",
+                secure: true,
+              });
 
-            setChecked(e.currentTarget.checked);
-          }}
-          checked={checked}
-          className={styles.checkbox}
-        >
-          ほかほかおにぎりクラブ会員専用コンテンツを非表示にする
-        </Checkbox>
-        {enabledPwa && !isPwa ? (
-          <button
-            className={styles.button}
-            disabled={!canInstallprompt || appinstalled}
-            onClick={showInstallPrompt}
+              setChecked(e.currentTarget.checked);
+            }}
+            checked={checked}
+            className={styles.checkbox}
           >
-            インストールする
-          </button>
-        ) : null}
-      </div>
+            ほかほかおにぎりクラブ会員専用コンテンツを非表示にする
+          </Checkbox>
+        </div>
+      </article>
+      {enabledPwa && !isPwa ? (
+        <article className={styles.article}>
+          <div className={styles.inner}>
+            <h2 className={styles.h2}>アプリ</h2>
+            <button
+              className={styles.button}
+              disabled={!canInstallprompt || appinstalled}
+              onClick={showInstallPrompt}
+            >
+              インストールする
+            </button>
+          </div>
+        </article>
+      ) : null}
     </div>
   );
 }
